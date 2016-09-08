bd8afd8e28125f396eb29cefb779bfb4baec2403
Chris Martinez and Katy Henning <chris+katy@>
2016-09-08 10:02:22 -0700

    begin car dealership page with app.php and two templates for form and display

--- app/app.php
+++ app/app.php
@@ -1,12 +1,32 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__.'/../src/car.php';
+    require_once __DIR__.'/../src/Car.php';
+    date_default_timezone_set('America/Los_Angeles');
+    session_start();
+
+    if (empty($_SESSION['allCars'])){
+      $_SESSION['allCars'] = array();
+    }
 
     $app = new Silex\Application();
 
-    $app->get("/", function() {
-        return "Home";
-    });
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+
+      if(!empty($_SESSION['allCars'])){
+        var_dump($_SESSION['allCars']);
+      }
+      return $app['twig']->render('home.html.twig', array('car'=>Car::getAll()));
+     });
+
+     $app->post("/new_car", function() use ($app) {
+       $new_car = new Car($_POST['make'], $_POST['miles'], $_POST['image'], $_POST['price']);
+       $new_car->save();
+       return $app['twig']->render('newcaradd.html.twig', array('car' => $new_car));
+      });
 
     return $app;
 ?>
--- src/car.php
+++ src/car.php
@@ -3,70 +3,66 @@
 
 class Car
 {
-    private $make_model;
-    private $price;
-    private $miles;
+    public $make_model;
+    public $price;
+    public $miles;
     public $image;
-    function __construct($make_model,  $price = 0, $miles=4, $image )
+    function __construct($make_model, $price, $miles, $image)
     {
         $this->make_model = $make_model;
         $this->price = $price;
         $this->miles = $miles;
         $this->image = $image;
     }
-    function setPrice ($new_price)
+    function setPrice($new_price)
     {
         $float_price =(float) $new_price;
         if($float_price != 0){
             $formatted_price = number_format($float_price, 2);
             $this->price = $formatted_price;
         }
-
     }
-    function getPrice ()
+    function getPrice()
     {
         return $this->price;
 
     }
-    function setModel ($new_model)
+    function setModel($new_model)
     {
         if($new_model){
             $this->make_model = $new_model;
         }
 
     }
-    function getModel ()
+    function getModel()
     {
         return $this->make_model;
 
     }
-    function setMiles ($new_miles)
+    function setMiles($new_miles)
     {
         if($new_miles){
             $this->miles = $new_miles;
         }
     }
-    function getMiles ()
+    function getMiles()
     {
         return $this->miles;
 
     }
-}
-
-$porsche = new Car("2014 Porsche 911", 114991, 7864,"../img/porsche-model.png");
-$ford = new Car("2011 Ford F450", 55995,55995, "../img/ford.png");
-$lexus = new Car("2013 Lexus RX 350",  20000, 8990809, "../img/lexus.png");
-$mercedes = new Car("Mercedes Benz CLS550",8980, 2, "../img/mercedes.png");
-
-
-$cars = array($porsche, $ford, $lexus, $mercedes);
-
-$cars_matching_search = array();
-foreach ($cars as $car) {
-    $price =  $car->getPrice();
-    $miles = $car->getMiles();
-    if ($miles <  $_GET["input_miles"] && $price < $_GET["input_price"]) {
-        array_push($cars_matching_search, $car);
+    function save()
+    {
+      array_push($_SESSION["allCars"], $this);
+    }
+    static function getAll()
+    {
+        return $_SESSION['allCars'];
+    }
+    static function deleteAll()
+    {
+        $_SESSION['allCars'] = array();
     }
 }
+
+
 ?>
